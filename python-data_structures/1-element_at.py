cb622a103ca391f5bc3532745fb7882c9a9c7ab0
root <luismichelpcontacto@gmail>
2025-05-14 03:21:59 +0000

    1-element_at.py

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx not in my_list:
+        return None
+    else
+    return "Element at index {:d} is {}".format(idx, element_at(my_list, idx))
